9b83017cf5893745d0a102778ae6e15cfc07425b
evenllcsouser <118425330+evenllcsouser@users.noreply.github.com>
2022-12-01 16:10:45 +0900

    Update autotrade.py

--- autotrade.py
+++ autotrade.py
@@ -2,8 +2,8 @@ import pyupbit
 import schedule
 import time
 
-access = "ufjmSghkGw401rx4PTkd2xxhxSwbRFQGS9UsOOxh"          # 본인 값으로 변경
-secret = "nYFef54KjxPeghU9bgKjNyy90Yqi9FqfLsUcxSgy"          # 본인 값으로 변경
+access = ""          # 본인 값으로 변경
+secret = ""          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-BTC"))     # KRW-BTC 조회
@@ -14,7 +14,7 @@ def job(): # 아래 것을 주기적으로 실행
     a = upbit.get_balance("KRW")# -5000
     b = upbit.get_balance("KRW-BTC") * pyupbit.get_current_price("KRW-BTC")
     c = upbit.get_balance("KRW-XRP") * pyupbit.get_current_price("KRW-XRP")
-    d = upbit.get_balance("KRW-DOGE") * pyupbit.get_current_price("KRW-DOGE")
+    d = upbit.get_balance("KRW-ETH") * pyupbit.get_current_price("KRW-ETH")
     e = a + b + c+ d
     
     if b/e < 0.7 :                                           # 참이려면 비트코인 가격이 하락하였을 때지
@@ -30,10 +30,10 @@ def job(): # 아래 것을 주기적으로 실행
         upbit.sell_market_order("KRW-XRP", (c-0.15*e)/pyupbit.get_current_price("KRW-XRP"))       # XRP로
 
     if d/e < 0.15 :                                           # 참이려면 비트코인 가격이 하락하였을 때지
-        upbit.buy_market_order("KRW-DOGE", 0.15*e-d)     # 돈으로
+        upbit.buy_market_order("KRW-ETH", 0.15*e-d)     # 돈으로
     
     else :
-        upbit.sell_market_order("KRW-DOGE", (d-0.15*e)/pyupbit.get_current_price("KRW-DOGE"))       # DOGE로
+        upbit.sell_market_order("KRW-ETH", (d-0.15*e)/pyupbit.get_current_price("KRW-ETH"))       # ETH로
 
 # schedule.every().hour.do(job)
 # schedule.every(1).seconds.do(job)
